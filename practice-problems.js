9147b54ec2884b3fab3615a900bf86d77f547337
masudulalam <masuudulalam@gmail.com>
2024-04-22 04:31:16 +0600

    problem 1 soleved another way

--- practice-problems.js
+++ practice-problems.js
@@ -2,9 +2,16 @@
 const oddArray = [1 , 3, 5, 7, 9];
 // using for loop
 let evenArray = [];
-for (let i = 0; i < oddArray.length; i++) {
-    const evenNumber = oddArray[i] + 1;
+// for (let i = 0; i < oddArray.length; i++) {
+//     const evenNumber = oddArray[i] + 1;
+//     evenArray.push(evenNumber);
+// };
+// console.log(evenArray);
+
+// using for of loop
+for (const number of oddArray) {
+    const evenNumber = number + 1; 
     evenArray.push(evenNumber);
-};
+}
 console.log(evenArray);
 
